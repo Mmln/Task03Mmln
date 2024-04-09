449b5be29b6b44095431b5fc9c1ddb37dc72e935
Andrey Mamylin <andrey.mamylin@open.ru>
2024-04-10 00:33:54 +0500

    some constants added

--- src/main/java/com/orgexample/TimerImpl.java
+++ src/main/java/com/orgexample/TimerImpl.java
@@ -5,15 +5,19 @@ import java.math.BigDecimal;
 public class TimerImpl {
 
     private final long offset;
+    private final int countTimerImpl = 500;
+    private static final int oneMillion = 1_000_000;
+    private static final int oneThousand = 1_000;
+
 
     private static long calculateOffset() {
         final long nano = System.nanoTime();
-        final long nanoFromMilli = System.currentTimeMillis() * 1_000_000;
+        final long nanoFromMilli = System.currentTimeMillis() * oneMillion;
         return nanoFromMilli - nano;
     }
 
     public TimerImpl() {
-        final int count = 500;
+        final int count = countTimerImpl;
         BigDecimal offsetSum = BigDecimal.ZERO;
         for (int i = 0; i < count; i++) {
             offsetSum = offsetSum.add(BigDecimal.valueOf(calculateOffset()));
@@ -26,7 +30,7 @@ public class TimerImpl {
     }
 
     public long nowMicro() {
-        return (offset + System.nanoTime()) / 1000;
+        return (offset + System.nanoTime()) / oneThousand;
     }
 
     public long nowMilli() {
